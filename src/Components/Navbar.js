bd6422cc063f54592813755980d3d00f96a0ecc2
Lewis Leal <lewleal2012@gmail.com>
2020-08-29 11:23:40 -0400

    Navbar&Card

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex">
-                <a className="navbar-brand" href="#">Home</a>
+                <Link className="navbar-brand" to="#">Home</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Dropdown link
-        </a>
+        </Link>
                         </li>
                     </ul>
                 </div>
