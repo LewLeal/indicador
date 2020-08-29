00c70e21722d5411f7ee198f2e6174c1f76cffcf
Lewis Leal <lewleal2012@gmail.com>
2020-08-29 12:13:18 -0400

    Creacion de View-ComponenteDetalle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery'; import 'popper.js'; import 'bootstrap';
 import Navbar from './Components/Navbar';
 import Card from './Components/Card';
+import Detalle from './Components/Detalles';
+
+useEffect(() => {
+  fetch('https://aves.ninjas.cl/api/birds') 
+  .then ((resp) => resp.json())
+  .then ((datos) => {
+    console.log(datos)
+  })
+}, [])
 
 function App() {
   return (
     <>
     <Navbar/>
     <Card/>
+    <Detalle/>
    
      
     </>
