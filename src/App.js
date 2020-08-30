9701b07829256d3531b5f85c9a59b5276e24f0ea
Lewis Leal <lewleal2012@gmail.com>
2020-08-30 11:57:06 -0400

    correccion packjson

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery'; import 'popper.js'; import 'bootstrap';
 import Navbar from './Components/Navbar';
 import Card from './Components/Card';
 import Detalle from './Components/Detalles';
 
-useEffect(() => {
-  fetch('https://aves.ninjas.cl/api/birds') 
-  .then ((resp) => resp.json())
-  .then ((datos) => {
-    console.log(datos)
-  })
-}, [])
+
 
 function App() {
+  const [birds, setBirds] = useState()
+  useEffect(() => {
+    fetch('https://aves.ninjas.cl/api/birds')
+      .then((resp) => resp.json())
+      .then((datos) => {
+        setBirds (datos);
+        console.log(birds)
+      })
+  },)
   return (
     <>
-    <Navbar/>
-    <Card/>
-    <Detalle/>
-   
-     
+      <Navbar />
+      <Card />
+      <Detalle />
+
+
     </>
   );
 }
